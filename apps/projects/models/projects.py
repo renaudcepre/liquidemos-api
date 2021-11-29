7d7adfe185cb4bb991afd0564d9d9081a0ef4b43
Renaud Cepre <renaudcepre@gmail.com>
2021-11-29 21:47:50 +0100

    add creators to prajects and propostitions.

--- apps/projects/models/projects.py
+++ apps/projects/models/projects.py
@@ -2,12 +2,14 @@ from django.db import models
 
 from apps.commons.utils.model_mixins import DatedModelMixin
 from apps.projects.models.tag import Tag
+from apps.users.models import User
 
 
 class Alternative(DatedModelMixin, models.Model):
     """Alternative contains the real proposition, and can be 'forked' ad another alternative."""
     content = models.TextField()
     proposition = models.ForeignKey('Proposition', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Alternative {self.id} for  {self.proposition.name}"
@@ -17,6 +19,7 @@ class Proposition(models.Model):
     """A proposition is defined by these alternatives."""
     name = models.CharField(max_length=64)
     project = models.ForeignKey("Project", on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Proposition {self.name} ({self.alternative_set.count()} alternatives)"
@@ -26,6 +29,7 @@ class Project(DatedModelMixin, models.Model):
     """A project contains a number of propositions."""
     name = models.CharField(max_length=64)
     description = models.TextField(blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     from_proposition = models.ForeignKey(Proposition, on_delete=models.PROTECT,
                                          null=True, blank=True,
