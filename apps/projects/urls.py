3882724e96afb13bd9fe3fae89f021e4de961e63
Renaud Cepre <renaudcepre@gmail.com>
2021-11-30 16:07:19 +0100

    frontend

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from apps.projects.views import list_projects, project_detail
 
 urlpatterns = [
-    path('', list_projects),
-    path('<str:slug>', project_detail),
+    path('', list_projects, name='project_list'),
+    path('<str:slug>', project_detail, name='project_detail'),
 ]
