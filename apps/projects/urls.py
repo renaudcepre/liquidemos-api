029d4d027eb8b4c910ed81e121cfdbe3653a5820
Renaud Cepre <renaudcepre@gmail.com>
2021-12-09 09:52:57 +0100

    add paginations

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from apps.projects.views import list_projects, project_detail, proposition_detail
+from apps.projects.views import ProjectListView, project_detail, proposition_detail
 
 urlpatterns = [
-    path('', list_projects, name='project_list'),
+    path('', ProjectListView.as_view(), name='project_list'),
     path('<str:slug>', project_detail, name='project_detail'),
     path('proposition/<int:id>', proposition_detail, name='proposition_detail'),
 ]
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,18 +1,29 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from apps.projects.models import Project, Proposition
 
 
-def list_projects(request):
-    projects = Project.objects.all()
-    return render(request, 'project_list.html', context={'projects': projects})
+class ProjectListView(ListView):
+    paginate_by = 6
+    model = Project
 
 
 def project_detail(request, slug):
     project = get_object_or_404(Project, slug=slug)
-    return render(request, 'project_detail.html', context={'project': project})
+    proposition_list = project.proposition_set.all()
+    paginator = Paginator(proposition_list, 6)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'projects/project_detail.html',
+                  context={'project': project,
+                           'page_obj': page_obj,
+                           'paginator': paginator})
 
 
 def proposition_detail(request, id):
     proposition = get_object_or_404(Proposition, pk=id)
-    return render(request, 'proposition_detail.html', context={'proposition': proposition})
+    return render(request, 'projects/proposition_detail.html',
+                  context={'proposition': proposition})
