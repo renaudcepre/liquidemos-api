e0de871eea8ffc724b33debc1891aa1004f6a7e4
Renaud Cepre <renaudcepre@gmail.com>
2021-12-10 06:19:58 +0100

    add pagination util separated in block

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -8,12 +8,15 @@ from apps.projects.models import Project, Proposition
 class ProjectListView(ListView):
     paginate_by = 6
     model = Project
+    ordering = ['created_at']
 
 
 def project_detail(request, slug):
+    """La vue d'un projet et toutes ses propositions liées, paginées."""
     project = get_object_or_404(Project, slug=slug)
-    proposition_list = project.proposition_set.all()
-    paginator = Paginator(proposition_list, 6)
+
+    proposition_list = project.proposition_set.all().order_by('proposition__created_at')
+    paginator = Paginator(proposition_list, 3)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
