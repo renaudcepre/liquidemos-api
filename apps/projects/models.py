a3ea35070960b9cb4201a9ef6e221fa3aede9dbc
Renaud Cepre <renaudcepre@gmail.com>
2021-12-16 22:41:14 +0100

    [MODEL] : clean, create paths on save. default ordering.

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,14 +1,10 @@
 import logging
-import random
 from typing import List
 
-from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.db import models
-from django.db.models import Value
-from django.db.models.functions import Length, Replace
-from django.db.models.signals import m2m_changed
-from django.dispatch import receiver
+from django.db.models import QuerySet
+from django.db.models.functions import Length
 from django.utils.text import slugify
 
 from apps.commons.utils.model_mixins import DatedModelMixin
@@ -17,6 +13,19 @@ from apps.users.models import User
 logger = logging.getLogger(__name__)
 
 models.CharField.register_lookup(Length, 'length')
+
+
+def next_id(row: QuerySet):
+    node_ids: List[int] = [n.node_id for n in row]
+    assert node_ids == sorted(node_ids)
+
+    if len(node_ids) == 0 or node_ids[0] != 0:
+        return 0
+
+    for i, n in enumerate(node_ids):
+        if i > 0 and node_ids[i - 1] != node_ids[i] - 1:
+            return n - 1
+    return row.count()
 
 
 class Tag(models.Model):
@@ -53,78 +62,43 @@ class Project(DatedModelMixin, models.Model):
         self.slug = slugify(self.name)
 
         if not self.pk:
-            logger.info("New Node created, create path for it.")
-
-            # if self.parent is None:
-            #     tops = Project.objects.filter(path__length=1)
-            #     self.path = hex(tops.count())[2:]
-            #     self.depth = 0
-            # else:
-            #     self.depth = self.parent.depth + 1
-            #     self.path = self.parent.path + '/' + hex(max(
-            #         [c.row_index for c in Project.objects.filter(depth=self.depth)]))[2:]
+            if self.parent is None:
+                tops = Project.objects.filter(path__length=1)
+                free = next_id(tops)
+                self.path = hex(free)[2:]
+                self.depth = 0
+            else:
+                self.depth = self.parent.depth + 1
+                row = Project.objects.filter(depth=self.depth)
+                free = next_id(row)
+                self.path = self.parent.path + '/' + hex(free)[2:]
 
         super().save(*args, **kwargs)
 
     @property
-    def row_index(self) -> int:
+    def node_id(self) -> int:
+        """Parse and retrun the name of the node in the path """
         return int(self.path.rpartition('/')[2], 16)
 
-    def parents(self, depth=0):
-        paths = [self.path[0:x] for x in [i for i, c in enumerate(self.path) if c == '/']][-depth:]
-        qs = Project.objects.filter(path__in=paths).order_by('-path__length')
+    def parents(self, depth: int = 0) -> QuerySet:
+        """Query all childs of the node in the given depth, and return them as a queryset"""
+        path = self.path
+        # Create a list of all the parents paths
+        paths = [path[0:x] for x in [i for i, c in enumerate(path) if c == '/']][-depth:]
+
+        qs = Project.objects.filter(path__in=paths)
         return qs
 
-    # def childs(self, depth=0):
-    #     qs = Project.objects.filter(depth__gt=self.depth, path__startswith=self.path)
-    #     if depth > 0:
-    #         qs = Project.objects.filter(depth__lt=self.depth + depth + 1)
-    #     return qs.exclude(pk=self.pk)
-
-    def childs(self, depth=0):
+    def childs(self, depth: int = 0) -> QuerySet:
+        """Query all parents of the node in the given depth, and return them as a queryset"""
         qs = Project.objects.filter(path__startswith=self.path)
         if depth > 0:
             qs = qs.filter(depth__lt=self.depth + depth + 1)
-            # other tehcniques, slower but interresting:
-            # qs = qs.annotate(dd=Length('path') - Length(Replace('path', Value('/')))).filter(dd__lt=depth + 1)
-            # or
-            # reg = '^' + self.path + r'(/[A-Z0-9]+){,' + str(depth) + '}$'
-            # qs = qs.filter(path__regex=reg)
-
         return qs.exclude(pk=self.pk)
 
     def __str__(self):
         return self.path
-        # if not self.parent:
-        #     return f"{self.path}"
-        # if len(self.parents()) == 1:
-        #     return f"{self.parent.path}/{self.path}"
-        # return f"{self.parents()[-1].path}/.../{self.path}"
 
-# @receiver(m2m_changed, sender=Project.depends_on.through)
-# def validate_depends(sender, action, instance: Project, **kwargs):
-#     """Chek that the projects added as dependencies are not parents or alternatives."""
-#     if action == 'post_add':
-#         parent_list = instance.parents
-#         for rel in instance.depends_on.all():
-#             if rel == instance:
-#                 raise ValidationError(f"Can depends on itself")
-#             if rel in parent_list:
-#                 raise ValidationError(f'This project already depends on "{rel}" by inheritance.')
-#             if rel in instance.alternatives.all():
-#                 raise ValidationError(
-#                     f'{rel} cannot be added as dependencie, because its an alternative to that project')
-#
-#
-# @receiver(m2m_changed, sender=Project.alternatives.through)
-# def validate_alternatives(sender, action, instance: Project, **kwargs):
-#     """"""
-#     if action == 'post_add':
-#         for alternative in instance.alternatives.all():
-#             if alternative == instance:
-#                 raise ValidationError(f"Cannot be an alternative to itself")
-#             if alternative.parent != instance.parent:
-#                 raise ValidationError(f'Alternatives have to share the same parent')
-#             if alternative in instance.alternatives.all() or instance in alternative.alternatives.all():
-#                 raise ValidationError(
-#                     f'{alternative} cannot be added as alternative, because its a dependencie to that project')
+    class Meta:
+        # allow to have a list as '0, 1, 0/1, 0/2' instead of '0, 0/1, 0/2, 1'
+        ordering = ['path__length', 'path']
