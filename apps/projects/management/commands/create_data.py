b00fce50b602918073e8cf091cf9d2a6b54cbac4
Renaud Cepre <renaudcepre@gmail.com>
2021-12-16 16:17:39 +0100

    [MODEL] : Create a graph

---
+++ apps/projects/management/commands/create_data.py
@@ -0,0 +1,47 @@
+import random
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+from apps.projects.models import Project
+from apps.users.models import User
+
+
+class Command(BaseCommand):
+    help = 'Create initial project data'
+
+    def add_arguments(self, parser):
+        parser.add_argument('depth', nargs='+', type=int)
+        parser.add_argument('width', nargs='+', type=int)
+
+    def handle(self, *args, **options):
+        """Create a node tree with given depth and width"""
+        # Clear previous data
+        Project.objects.all().delete()
+
+        depth = options['depth'][0]
+        width = options['width'][0]
+
+        user = User.objects.get_or_create(username='rcepre', is_superuser=True)[0]
+
+        t1 = datetime.now()
+        # Create the top node.
+        first_node = Project.objects.create(parent=None, path='0', depth=0, created_by=user, name='0')
+        line = [first_node]
+
+        for d in range(depth):
+            print(f'depth: {d} .... ', end='')
+            new_line = []
+            for index, parent in enumerate(line):
+                parent.save()
+                for i in range(width):
+                    path = parent.path + '/' + hex(i)[2:]
+                    new_node = Project(
+                        parent=parent,
+                        path=path, depth=d + 1,
+                        created_by=user, name=path)
+
+                    new_line.append(new_node)
+            print(f"{len(new_line)} nodes.")
+            # Project.objects.bulk_create(new_line)
+            line = new_line
+        print(f"Created {Project.objects.count()} nodes [{(datetime.now() - t1).microseconds / 1000} ms]")
