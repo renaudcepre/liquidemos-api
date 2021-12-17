9960b4d2844a2d5bd47b2889a09c944287654e77
Renaud Cepre <renaudcepre@gmail.com>
2021-12-17 15:20:07 +0100

    [ORGA] : Move MaterializedPathNode into his own package.

---
+++ apps/commons/mptree/encoder.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+from functools import reduce
+
+import pytest
+
+
+class Encoder:
+    CHARSET_16 = '0123456789ABCDEF'
+    CHARSET_32 = '0123456789ABCDEFGHIJKLMNOPQRSTUV'
+    CHARSET_44 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`{}~'
+    CHARSET_64 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ~`!@#$%^&*()_-+={[]}:;<,>.?|'
+    DEFAULT_CHARSET = CHARSET_16
+
+    def __init__(self, charset: str = DEFAULT_CHARSET, case=True):
+        self.case = case
+        if not self.case:
+            charset = charset.lower()
+        assert len(set(charset)) == len(charset), 'Charset must contains only unique characters'
+
+        self.charset = charset
+        self.base = len(charset)
+
+    def encode(self, number: int) -> str:
+
+        result = ''
+
+        if number == 0:
+            return self.charset[0]
+        while number > 0:
+            tmp = number % self.base
+            result += self.charset[tmp]
+            number //= self.base
+
+        return result[::-1]
+
+    def decode(self, encoded: str) -> int:
+        if not self.case:
+            encoded = encoded.lower()
+        assert len(set(self.charset + encoded)) == len(self.charset), \
+            f"These characters are unknow in the instance charset: `{','.join(sorted(set(encoded) - set(self.charset)))}`"
+
+        return reduce(lambda count, c: count * self.base + self.charset.index(c), encoded, 0)
+
+
+def encode_decode(encoder: Encoder, number: int, target: str):
+    encoded = encoder.encode(number)
+    assert encoded == target
+    assert encoder.decode(encoded) == number
+
+
+def test_encode():
+    encoder = Encoder()
+    assert encoder.base == 16
+
+    encode_decode(encoder, 0, '0')
+    encode_decode(encoder, 65535, 'FFFF')
+    encode_decode(encoder, 65534, 'FFFE')
+    encode_decode(encoder, 27492339183, '666ABCDEF')
+
+    encoder = Encoder(charset='01')
+    assert encoder.base == 2
+
+    encode_decode(encoder, 0, '0')
+    encode_decode(encoder, 65535, '1111111111111111')
+    encode_decode(encoder, 42, '101010')
+
+    encoder = Encoder(charset='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ~`!@#$%^&*()_-+={[]}:;<,>.?/')
+    assert encoder.base == 64
+
+    encode_decode(encoder, 0, '0')
+    encode_decode(encoder, 63, '/')
+    encode_decode(encoder, 64, '10')
+    encode_decode(encoder, 4096, '100')
+    encode_decode(encoder, 35, 'Z')
+
+    encoder = Encoder(charset='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ~`!@#$%^&*()_-+={[]}:;<,>.?/', case=False)
+    encode_decode(encoder, 35, 'z')
+
+    encoder = Encoder(charset='XY')
+    assert encoder.base == 2
+
+    encode_decode(encoder, 0, 'X')
+
+    encoder = Encoder(charset='XY', case=False)
+    encode_decode(encoder, 0, 'x')
+    with pytest.raises(AssertionError):
+        Encoder(charset='aA', case=False)  # Two times the same char
+
+
+if __name__ == '__main__':
+    t1 = datetime.now()
+
+    test_encode()
+
+    print(f"{(datetime.now() - t1).microseconds / 1000} milliseconds")
--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,31 +1,17 @@
 import logging
-from typing import List
 
 from django.core.validators import RegexValidator
 from django.db import models
-from django.db.models import QuerySet
 from django.db.models.functions import Length
 from django.utils.text import slugify
 
-from apps.commons.utils.Encoder import Encoder
-from apps.commons.utils.model_mixins import DatedModelMixin
+from apps.commons.utilty.model_mixins import DatedModelMixin
 from apps.users.models import User
+from apps.commons.mptree.models import MaterializedPathNodeModel
 
 logger = logging.getLogger(__name__)
 
 models.CharField.register_lookup(Length, 'length')
-
-
-def next_id(row: QuerySet):
-    node_ids: List[int] = sorted([n.node_id for n in row])
-
-    if len(node_ids) == 0 or node_ids[0] != 0:
-        return 0
-
-    for i, n in enumerate(node_ids):
-        if i > 0 and node_ids[i - 1] != node_ids[i] - 1:
-            return n - 1
-    return row.count()
 
 
 class Tag(models.Model):
@@ -35,15 +21,7 @@ class Tag(models.Model):
         return f"{self.name}"
 
 
-class Project(DatedModelMixin, models.Model):
-    encoder = Encoder(charset=Encoder.CHARSET_44)
-    assert not encoder.charset.find('/') != -1
-    # MATERIALIZED PATH TREE
-    path = models.CharField(max_length=1024, db_index=True, unique=True, editable=False)
-    depth = models.IntegerField(editable=False, null=False)
-    parent = models.ForeignKey('Project', on_delete=models.CASCADE, null=True, blank=True, related_name='fk_childs')
-
-    # PROJECT
+class Project(DatedModelMixin, MaterializedPathNodeModel):
     name = models.CharField(max_length=64,
                             validators=[RegexValidator(r'^[a-zA-Z-1-9-_ ]*$')])
     description = models.TextField(blank=True)
@@ -52,55 +30,12 @@ class Project(DatedModelMixin, models.Model):
 
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    # Ne doivent pas etre ni parents, ni alternatives
     depends_on = models.ManyToManyField("Project",
                                         related_name='dependencies',
                                         blank=True)
-    # Les alternatives doivent avoir le meme parent, et ne pas etre dependants
-    # les unes des autres
-    alternatives = models.ManyToManyField("Project", blank=True)
-
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-
-        if not self.pk:
-            if self.parent is None:
-                tops = Project.objects.filter(parent__isnull=True)
-                free = next_id(tops)
-                self.path = Project.encoder.encode(free)
-                self.depth = 0
-            else:
-                self.depth = self.parent.depth + 1
-                row = Project.objects.filter(depth=self.depth)
-                free = next_id(row)
-                self.path = self.parent.path + '/' + Project.encoder.encode(free)
         super().save(*args, **kwargs)
 
-
-    @property
-    def node_id(self) -> int:
-        """Parse and retrun the name of the node in the path """
-        return Project.encoder.decode(self.path.rpartition('/')[2])
-
-    def parents(self, depth: int = 0) -> QuerySet:
-        """Query all childs of the node in the given depth, and return them as a queryset"""
-        path = self.path
-        # Create a list of all the parents paths
-        paths = [path[0:x] for x in [i for i, c in enumerate(path) if c == '/']][-depth:]
-
-        qs = Project.objects.filter(path__in=paths)
-        return qs
-
-    def childs(self, depth: int = 0) -> QuerySet:
-        """Query all parents of the node in the given depth, and return them as a queryset"""
-        qs = Project.objects.filter(path__startswith=self.path)
-        if depth > 0:
-            qs = qs.filter(depth__lt=self.depth + depth + 1)
-        return qs.exclude(pk=self.pk)
-
     def __str__(self):
-        return self.path
-
-    class Meta:
-        # allow to have a list as '0, 1, 0/1, 0/2' instead of '0, 0/1, 0/2, 1'
-        ordering = ['path__length', 'path']
+        return self.slug
