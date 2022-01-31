af7f80a2e784a7e2c7450e2cd44744058b9db3c2
Renaud Cepre <renaudcepre@gmail.com>
2022-01-31 11:13:28 +0100

    [TEST] : rewriting

--- apps/projects/tests/test_models.py
+++ apps/projects/tests/test_models.py
@@ -1,42 +1,56 @@
+import logging
+import uuid
+
 import pytest
-from django.db.models import QuerySet
+from django.core.management import call_command
+from django.utils.text import slugify
 
-from apps.projects.models import Project, AlternativeGroup, Vote
-from apps.users.models import User
+from apps.projects.models import Project, AlternativeGroup, Vote, Tag, Delegation
 
-
-@pytest.fixture
-def superuser() -> User:
-    return User.objects.create_superuser(username='admin',
-                                         email='admin@admin.com',
-                                         password='password')
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture
-def project_tree(superuser) -> QuerySet:
-    root = Project(name='This is a test', parent=None,
-                   created_by=superuser)
-    root.save()
+def create_user(db, django_user_model):
+    """Return a closure that create a user with random values and default password."""
 
-    for i in range(3):
-        Project(name=f"child n{i}", parent=root,
-                created_by=superuser).save()
+    def _make_user(**kwargs):
+        kwargs['password'] = 'test_password'
+        if 'username' not in kwargs:
+            kwargs['username'] = str(uuid.uuid4())
+        return django_user_model.objects.create_user(**kwargs)
 
-    Project(name='child', parent=root.childs().first(),
-            created_by=superuser).save()
-    assert Project.objects.count() == 5
-    return Project.objects.all()
+    return _make_user
 
 
-@pytest.mark.django_db
-def test_project_model(superuser):
-    p = Project(name='This is a test', parent=None, created_by=superuser)
+@pytest.fixture
+def create_project(db, create_user):
+    """Return a closure that create a project with random values."""
+
+    def _make_project(**kwargs):
+        if 'name' not in kwargs:
+            kwargs['name'] = str(uuid.uuid4())
+        if 'created_by' not in kwargs:
+            kwargs['created_by'] = create_user()
+        return Project.objects.create(**kwargs)
+
+    return _make_project
+
+
+@pytest.fixture(scope='session')
+def project_tree(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command('loaddata', 'test_projects.json')
+
+
+def test_project_model(create_project):
+    p = create_project(name='test project')
     p.save()
     assert p.path == '0'
     assert p.node_id == 0
     assert p.depth == 0
-    assert p.name == 'This is a test'
-    assert p.slug == 'this-is-a-test'
+    assert p.name == 'test project'
+    assert p.slug == slugify('test project')
     assert p.parent is None
     assert p.parents().count() == 0
     assert p.childs().count() == 0
@@ -44,47 +58,54 @@ def test_project_model(superuser):
 
 @pytest.mark.django_db
 def test_tree_direct_childs(project_tree):
-    root = project_tree.get(depth=0)
+    # get the top of the tree
+    root = Project.objects.get(depth=0)
 
     assert root.childs(1).count() == 3
 
     for idx, child in enumerate(root.childs(1).all()):
         assert child.parent == root
         assert child.parents().count() == 1
-        assert child.parents().first() == root
         assert child.node_id == idx
         assert child.path == f'0/{idx}'
         assert child.depth == 1
 
 
 @pytest.mark.django_db
-def test_tree_childs_depth(project_tree):
-    root = project_tree.get(depth=0)
+def test_tree_grand_childs(project_tree):
+    childs = Project.objects.filter(depth=2)
 
-    child = root.childs().last()
-    assert child.path == '0/0/0'
-    assert child.node_id == 0
-    assert child.depth == 2
-    assert child.parents().count() == 2
-    assert child.parents(1).first() == child.parent
-    assert root.childs().count() == 4
-    assert root.childs(1).count() == 3
+    for idx, child in enumerate(childs):
+        # assert child.parent == root
+        assert child.parents().count() == 2
+        assert child.node_id == idx
+        assert child.path.startswith("0/")
+        assert child.path.endswith(str(idx))
+        assert child.depth == 2
+
+
+@pytest.mark.django_db
+def test_retrieve_parents(project_tree):
+    child = Project.objects.last()
+    assert child.parents().count() == child.depth == 3
+    assert child.path.startswith(child.parent.path)
 
 
 @pytest.mark.django_db
 def test_concurency_group(project_tree):
-    childs = project_tree.filter(depth=1)
+    childs = Project.objects.filter(depth=1)
     concurency_group = AlternativeGroup.objects.create()
     for child in childs:
         child.alternative_group = concurency_group
         child.save()
+        assert concurency_group.project_set.contains(child)
 
     assert concurency_group.project_set.count() == 3
 
 
 @pytest.mark.django_db
 def test_include_self(project_tree):
-    root: Project = project_tree.get(depth=0)
+    root: Project = Project.objects.get(depth=0)
 
     assert not root.childs().contains(root)
     assert root.childs(include_self=True).contains(root)
@@ -95,9 +116,10 @@ def test_include_self(project_tree):
 
 
 @pytest.mark.django_db
-def test_vote(superuser):
-    project = Project.objects.create(created_by=superuser, parent=None, name="test")
-    vote = Vote.objects.create(project=project, user=superuser)
+def test_vote(admin_user, create_user, create_project):
+    project = create_project(created_by=admin_user)
+    user = create_user()
+    vote = Vote.objects.create(project=project, user=user)
 
     assert project.upvotes == 1
     assert project.downvotes == 0
@@ -108,4 +130,17 @@ def test_vote(superuser):
     assert project.upvotes == 0
     assert project.downvotes == 1
 
-    assert vote.user == superuser
+    assert vote.user == user
+
+
+def test_delegation(create_user):
+    tag = Tag.objects.create(name='TAG')
+    delegate = create_user()
+    delegator = create_user()
+    deleg = Delegation.objects.create(tag=tag,
+                                      delegate=delegate,
+                                      delegator=delegator)
+
+    assert Delegation.objects.count() == 1
+    assert Delegation.get_delegations(delegate, tag).count() == 1
+    assert Delegation.get_delegations(delegator, tag).count() == 0
