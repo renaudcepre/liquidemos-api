0511acc8947346db40422d466a40ece562edd480
Renaud Cepre <renaudcepre@gmail.com>
2022-02-04 23:30:36 +0100

    [DEV_ENV] : query_info verbose

--- scratches/utils.py
+++ scratches/utils.py
@@ -4,22 +4,32 @@ import time
 from django.db import connection, reset_queries
 
 
-def query_debugger(func):
-    @functools.wraps(func)
-    def inner_func(*args, **kwargs):
-        reset_queries()
+def query_info(show_sql=False):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            reset_queries()
 
-        start_queries = len(connection.queries)
+            start_queries_count = len(connection.queries)
+            start = time.perf_counter()
 
-        start = time.perf_counter()
-        result = func(*args, **kwargs)
-        end = time.perf_counter()
+            result = func(*args, **kwargs)
 
-        end_queries = len(connection.queries)
+            end = time.perf_counter()
 
-        print(f"Function : {func.__name__}")
-        print(f"Number of Queries : {end_queries - start_queries}")
-        print(f"Finished in : {(end - start):.4f}s")
-        return result
+            end_queries = connection.queries
+            end_queries_count = len(end_queries)
 
-    return inner_func
+            print(f"for function \"{func.__name__}\":")
+            queries_count = end_queries_count - start_queries_count
+            print(
+                f"{queries_count} {'queries' if queries_count > 1 else 'query'} performed.")
+            if show_sql:
+                for q in end_queries:
+                    print(f"{q['time']}s - {q['sql']}")
+            print(f"Total: {(end - start):.4f}s")
+            return result
+
+        return wrapper
+
+    return decorator
