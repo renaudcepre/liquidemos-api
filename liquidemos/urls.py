f39dfa5fe329e5e4d1261a975f4992ac33af1e6d
Renaud Cepre <renaudcepre@gmail.com>
2022-02-07 22:02:50 +0100

    [DEPS] : Install DRF

--- liquidemos/urls.py
+++ liquidemos/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('projects/', include('apps.projects.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
