388d87984625c6b84add21a54c53958e8c5bb6ad
Renaud Cepre <renaudcepre@gmail.com>
2022-03-01 11:59:35 +0100

    [TEST] : fix

--- tests/test_projects/test_e2e.py
+++ tests/test_projects/test_e2e.py
@@ -28,12 +28,6 @@ class TestVotes:
         assert response.status_code == status.HTTP_200_OK
         assert project.vote_set.count() == 1
 
-        # Les votes deja effectues ne sont pas mis à jour,
-        # Pour le projet sur lequel on vient de voter,
-        # requeter la liste des delegations sortantes pour le theme du projet,
-        # en sortir tous les votes effectues par les deleges sur ces delegations,
-        # mettre a jour ces votes.
-
         assert user.vote_weight(project2) == 3
         assert user.vote_weight(project) == 2
 
@@ -43,44 +37,3 @@ class TestVotes:
         assert user3.vote_weight(project2) == 1
         assert user3.vote_weight(project) == 1
 
-    # @pytest.mark.django_db
-    # def test_test(self, create_project, registered_user, api_client):
-    #     user, user2, user3 = registered_user(number=3)
-    #     project, project2 = create_project(number=2, created_by=user)
-    #     Delegation.objects.bulk_create((
-    #         Delegation(delegator=user2, delegate=user, theme=project.theme),
-    #         Delegation(delegator=user3, delegate=user2, theme=project.theme)))
-    #
-    #     log_user(api_client, user)
-    #     response = api_client.post(self.get_vote_url(project),
-    #                                data={'upvote': True})
-    #
-    #     log_user(api_client, user3)
-    #     response = api_client.post(self.get_vote_url(project),
-    #                                data={'upvote': True})
-    #
-    #     assert response.status_code == status.HTTP_200_OK
-    #     assert project.vote_set.count() == 1
-    #
-    #     # Les votes deja effectues ne sont pas mis à jour,
-    #     # Pour le projet sur lequel on vient de voter,
-    #     # requeter la liste des delegations sortantes pour le theme du projet,
-    #     # en sortir tous les votes effectues par les deleges sur ces delegations,
-    #     # mettre a jour ces votes.
-    #
-    #     assert user.vote_weight(project2) == 3
-    #     assert user.vote_weight(project) == 2
-    #
-    #     assert user2.vote_weight(project2) == 2
-    #     assert user2.vote_weight(project) == 1
-    #
-    #     assert user3.vote_weight(project2) == 1
-    #     assert user3.vote_weight(project) == 1
-    #
-    #     response = api_client.delete(self.get_vote_url(project))
-    #
-    #     assert response.status_code == status.HTTP_204_NO_CONTENT
-    #
-    #     assert user.vote_weight(project) == 3
-    #     assert user2.vote_weight(project) == 2
-    #     assert user3.vote_weight(project) == 1
